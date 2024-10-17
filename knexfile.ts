b214b37d7c263528c111a007d77391fbb086a153
LongDevLor <tranthoilong@gmail.com>
2024-10-17 19:14:37 +0700

    init db

--- knexfile.ts
+++ knexfile.ts
@@ -1,34 +1,12 @@
-// import { Knex } from 'knex';
-//
-// const config: Knex.Config = {
-//     client: 'pg',
-//     connection: {
-//         host: 'localhost',
-//         user: 'admin',
-//         password: 'dev_lor',
-//         database: 'ecommerce',
-//         port: 5432,
-//     },
-//     migrations: {
-//         directory: './migrations',
-//     },
-//     seeds: {
-//         directory: './seeds',
-//     },
-// };
-//
-// export default config;
-
-
 import { Knex } from 'knex';
 
 const config: Knex.Config = {
     client: 'pg',
     connection: {
-        host: 'dpg-cs8a9k5svqrc73bmvr10-a.singapore-postgres.render.com',
-        user: 'longdevlor',
-        password: 'G8A9fEIpTop0Z6R5vtD3YeexijH8jC10',
-        database: 'longdev',
+        host: 'localhost',
+        user: 'admin',
+        password: 'dev_lor',
+        database: 'ecommerce',
         port: 5432,
     },
     migrations: {
@@ -40,3 +18,6 @@ const config: Knex.Config = {
 };
 
 export default config;
+
+
+
