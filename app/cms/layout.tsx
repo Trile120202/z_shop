bd7a34abbba972bc459337c33c65a8803594cb88
LongDevLor <tranthoilong@gmail.com>
2024-10-19 17:52:07 +0700

    init

--- app/cms/layout.tsx
+++ app/cms/layout.tsx
@@ -6,12 +6,10 @@ export default function CMSLayout({
   return (
     <div className="cms-layout">
       <nav className="cms-sidebar">
-        {/* Thêm các mục menu quản trị ở đây */}
         <ul>
           <li>Dashboard</li>
           <li>Users</li>
           <li>Content</li>
-          {/* ... */}
         </ul>
       </nav>
       <main className="cms-main">{children}</main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-100`}>{children}</body>
+      <body className={`${inter.className} bg-gray-100`}>
+      <Header />
+
+      {children}
+      <Footer />
+
+      </body>
     </html>
   );
 }
