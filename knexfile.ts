c451615f51327a953ab5f4dff9d139427319ab8e
LongDevLor <tranthoilong@gmail.com>
2024-10-19 21:15:00 +0700

    api category

--- knexfile.ts
+++ knexfile.ts
@@ -3,14 +3,7 @@ import { Knex } from 'knex';
 const config: Knex.Config = {
     client: 'pg',
     connection: {
-        host: 'dpg-cs8a9k5svqrc73bmvr10-a.singapore-postgres.render.com',
-        user: 'longdevlor',
-        password: 'G8A9fEIpTop0Z6R5vtD3YeexijH8jC10',
-        database: 'longdev',
-        port: 5432,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+
     },
     migrations: {
         directory: './migrations',
