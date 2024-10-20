f7db2931e818ea791d5bf0a1d806cd2db2c69560
LongDevLor <tranthoilong@gmail.com>
2024-10-20 08:11:30 +0700

    text

--- app/components/home/CategoryHome.tsx
+++ app/components/home/CategoryHome.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import useFetch from "@/lib/useFetch";
-import Loading from "@/app/components/Loading";
 
 interface Category {
   image_url: string;
